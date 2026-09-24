3d5ac78e7485efa0201a26e3142dd56c0423bca7
jc842 <jc@les4h.fr>
2026-09-24 16:03:29 -0400

    feat(ads): synchronize unique Revive Adserver zone IDs

--- src/config/site.config.ts
+++ src/config/site.config.ts
@@ -49,9 +49,12 @@ export const siteConfig: SiteConfig = {
     reviveId: "ac119b122a644588953c74c4c1daee06",
     scriptUrl: "//ads.les4h.fr/www/delivery/asyncjs.php",
     zones: {
-      mobileSticky: { zoneId: 623, format: "mobileBanner" },
-      inContent: { zoneId: 627, format: "mediumRectangle" },
-      header: { zoneId: 626, format: "leaderboard" },
+      mobileSticky: { zoneId: 656, format: "mobileBanner" },
+      inContent: { zoneId: 655, format: "mediumRectangle" },
+      header: { zoneId: 654, format: "leaderboard" },
+      halfPage: { zoneId: 652, format: "halfPage" },
+      largeRectangle: { zoneId: 653, format: "largeRectangle" },
+      skyscraper: { zoneId: 657, format: "skyscraper" },
     },
   },
 
